2437dd7a1728fe73c445b33b0ae87295ef97ef59
acarrou <dridriswim@gmail.com>
2024-02-06 19:10:14 -0800

    :bug: fixes trans_header for libhal

--- conanfile.py
+++ conanfile.py
@@ -58,12 +58,13 @@ class libhal_mpl_conan(ConanFile):
             check_min_cppstd(self, self._min_cppstd)
 
     def build_requirements(self):
+        self.tool_requires("cmake/3.27.1")
         self.tool_requires("libhal-cmake-util/1.0.0")
-        self.test_requires("libhal-mock/[^2.0.0]")
+        self.test_requires("libhal-mock/[^2.0.1]")
         self.test_requires("boost-ext-ut/1.1.9")
 
     def requirements(self):
-        self.requires("libhal/[^2.0.0]")
+        self.requires("libhal/2.2.0", transitive_headers=True)
         self.requires("libhal-util/[^3.0.0]")
 
     def layout(self):
